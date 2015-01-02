620bace1b90beeb9826a15eccaf582b7f23254bb
Ralph <beresfordjunior@me.com>
2015-01-02 10:28:39 +0100

    working on apache option

--- wp-vagrant/bootstrap_old_nginx.sh
+++ wp-vagrant/bootstrap_old_nginx.sh
@@ -13,15 +13,19 @@ debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password password
 debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password_again password $mysql_root_password"
 
 apt-get update
-# apt-get install -qy nginx php5 php5-fpm php5-gd php5-mysql php5-cgi php5-cli php5-curl php5-xdebug phpunit vim git-core mysql-server-5.5 mysql-client curl byobu
-apt-get install -qy apache2 php5 php5-fpm php5-gd php5-mysql php5-cgi php5-cli php5-curl php5-xdebug phpunit vim git-core mysql-server-5.5 mysql-client curl byobu
+apt-get install -qy nginx php5 php5-fpm php5-gd php5-mysql php5-cgi php5-cli php5-curl php5-xdebug phpunit vim git-core mysql-server-5.5 mysql-client curl byobu
 
 
 echo "**** add byobu config"
 . /vagrant/wp-vagrant/configs/byobu.sh
 
-# echo "**** Moving nginx config files into place…"
-# . /vagrant/wp-vagrant/nginx/nginx.sh
+echo "**** Moving nginx config files into place…"
+. /vagrant/wp-vagrant/nginx/nginx.sh
+
+echo "Starting services…"
+service nginx restart
+service php5-fpm restart
+service mysql restart
 
 echo "**** mysql config…"
 mv /etc/mysql/my.cnf /etc/mysql/my.cnf.default
@@ -33,8 +37,7 @@ cp /vagrant/wp-vagrant/php/php.ini /etc/php5/fpm/php.ini
 sudo cp /vagrant/wp-vagrant/php/20-xdebug.ini /etc/php5/fpm/conf.d/
 
 echo "Starting services…"
-# service nginx restart
-service apache2 restart
+service nginx restart
 service php5-fpm restart
 service mysql restart
 
@@ -46,7 +49,7 @@ service mysql restart
 . /vagrant/wp-vagrant/mysql/create_database.sh
 
 # Install WP
-# . /vagrant/wp-vagrant/wp/install-wp.sh
+. /vagrant/wp-vagrant/wp/install-wp.sh
 
 # Import database
-# . /vagrant/wp-vagrant/mysql/import_database.sh
+. /vagrant/wp-vagrant/mysql/import_database.sh
